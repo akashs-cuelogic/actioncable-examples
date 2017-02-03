a9f76b5581dcbe70da148c780d5860d5c56bc842
Akash Shah <akash.shah@cuelogic.co.in>
2017-02-03 13:15:44 +0530

    initial commit

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,21 @@ class Comment < ActiveRecord::Base
   belongs_to :message
   belongs_to :user
 
-  after_commit { CommentRelayJob.perform_later(self) }
+  # after_create_commit { CommentRelayJob.perform_later(self) }
+  after_create_commit :send_create_data
+  after_update_commit :send_after_data
+  after_destroy_commit  :send_remove_data
+
+  def send_create_data
+    puts "send_create_data"
+    CommentRelayJob.perform_later(self)
+  end
+
+  def send_after_data
+    puts "send_after_data"
+  end
+
+  def send_remove_data
+    puts "send_remove_data"
+  end
 end
